69b84ec43568a999eb283179f8ab7c75a257e48f
Radiant213 <galangmaruff1@gmail.com>
2026-03-20 14:43:20 +0700

    Fix Vercel 500 Error temp directories

--- api/index.php
+++ api/index.php
@@ -1,5 +1,16 @@
 <?php
 
-// Entri point untuk Vercel Serverless Function
+// Pastikan folder-folder temp yang dibutuhkan Laravel dibikin otomatis di Vercel (karena /tmp itu kosong setiap ada request baru)
+$tempDirectories = [
+    '/tmp/storage/framework/views',
+    '/tmp/bootstrap/cache'
+];
+
+foreach ($tempDirectories as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0777, true);
+    }
+}
+
 // Meneruskan request ke public/index.php bawaan Laravel
 require __DIR__ . '/../public/index.php';

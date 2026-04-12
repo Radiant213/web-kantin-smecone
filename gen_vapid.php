c8512067eec182fec8c0c1492bf83f919c070d30
Radiant213 <galangmaruff1@gmail.com>
2026-04-12 21:38:13 +0700

    Clean up unused deployment checklists and vapid generator scripts

--- gen_vapid.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$keys = Minishlink\WebPush\VAPID::createVapidKeys();
-echo 'PUBLIC=' . $keys['publicKey'] . PHP_EOL;
-echo 'PRIVATE=' . $keys['privateKey'] . PHP_EOL;
